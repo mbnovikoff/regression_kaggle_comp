d69297c89352101ed46269d4d2fe5e3c810f593e
Morgan Novikoff <morgannovikoff@Morgans-MacBook-Pro-2.local>
2021-05-26 21:50:18 -0500

    Initial

--- Novikoff_Morgan_Regression_Comp.R
+++ Novikoff_Morgan_Regression_Comp.R
@@ -12,47 +12,52 @@ tidymodels_conflicts()
 set.seed(123) 
 
 # Load data ----
-# Make the outcome variable a factor in the data set 
-# import data
+
+# Convert character into factors 
 train <- read_csv("data/train.csv") %>% 
   mutate_if(is.character, as.factor)
- test <- read_csv("data/test.csv")
+test <- read_csv("data/test.csv")
 
+# Visualize important continous variables 
 train %>%
   select(-c(where(is.factor), where(is.character), id)) %>%
   cor(use = "pairwise") %>%
   corrplot::corrplot(method = "circle")
 
+# Folds
 folds <- vfold_cv(train,  v = 5, repeats = 3)
 
-bank_recipe <- recipe(money_made_inv ~ out_prncp_inv + loan_amnt + acc_now_delinq + num_tl_120dpd_2m + avg_cur_bal + tot_cur_bal + num_tl_30dpd + dti + int_rate + application_type
-                      + tot_coll_amt + term + home_ownership + annual_inc + grade, data = train) %>%
-  
-  #term + out_prncp_inv + int_rate + application_type +
-  #+  loan_amnt + tot_coll_amt 
-  #+ annual_inc + avg_cur_bal + home_ownership + dti + grade, data = train) %>%
-  #step_rm(id, purpose, earliest_cr_line, emp_title, last_credit_pull_d) %>% 
-  step_dummy(all_nominal())
+# Recipe
+bank_recipe <- recipe(money_made_inv ~ out_prncp_inv + loan_amnt + acc_now_delinq 
+                      + num_tl_120dpd_2m + avg_cur_bal + tot_cur_bal + num_tl_30dpd 
+                      + dti + int_rate + application_type
+                      + tot_coll_amt + term + home_ownership + annual_inc + grade, 
+                       data = train) %>%
+               step_dummy(all_nominal())
 
-#we canuse prep and juice to verify that our recipe is working and transforming our results as we want it to  
+# Prep and Bake  
 prep(bank_recipe) %>% 
   bake(new_data = NULL)
 
+# Random Forest Model
 rf_model <- rand_forest(mode = "regression",
                         min_n = tune(),
                         mtry = tune()) %>% 
-  set_engine("ranger")
+            set_engine("ranger")
 
+# Parameters
 rf_params <- parameters(rf_model) %>% 
   update(mtry = mtry(range = c(1, 15)))
 
+# Grid
 rf_grid <- grid_regular(rf_params, levels = 10)
 
+# Workflow
 rf_workflow <- workflow() %>% 
   add_model(rf_model) %>% 
   add_recipe(bank_recipe)
 
-# Tuning/fitting ----
+# Tune and Fit
 rf_tuned <- rf_workflow %>% 
   tune_grid(folds, grid = rf_grid)
 
@@ -61,8 +66,10 @@ rf_workflow_tuned <- rf_workflow %>%
 
 save(rf_tuned, rf_workflow, file = "model_info/rf_tuned.rda") 
 
+# Results 
 rf_results <- fit(rf_workflow_tuned, train)
 
+# Final Output
 rf_pred <- predict(rf_results, test) %>% 
   bind_cols(Id = test$id) %>% 
   rename(Predicted = .pred)
